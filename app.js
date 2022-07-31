6e1e00e706b80dae2cb88bca3f75d5c84bdc9a1a
miguelciancio <mciancio.figueiredo@gmail.com>
2022-07-31 11:53:07 +0100

    Modifying Elements & Classes JS

--- app.js
+++ app.js
@@ -28,9 +28,37 @@ for (i = 0; i < listItems.length; i++) {
 
 */
 
+const ul = window.document.querySelector('ul');;
+const li = window.document.createElement('li');
+
+// Adding Elements
+ul.append(li);
+
+// Modifying the text
+li.innerText = 'X-men';
+
+// Modifying attributes & classes
+
+/*
+// First method to add attribute / remove attribute.
+li.setAttribute('id', 'main-heading');
+li.removeAttribute('id');
+
+// Second method to get an attribute.
+const title = window.document.querySelector('#main-heading');
+
+console.log(title.getAttribute('id'));
+*/
+
+/*
+// Add a class attribute / remove attribute
+li.classList.add('list-items');
+li.classList.remove('list-items');
+
+console.log(li.classList.contains('list-items'));
+*/
+
+// Remove Elements
+li.remove()
 
 
-
-
-
-console.log();
