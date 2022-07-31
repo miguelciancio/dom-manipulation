093daf18c04ef1e40f1e2b125a3e9afba39799f1
miguelciancio <mciancio.figueiredo@gmail.com>
2022-07-31 09:42:44 +0100

    Selection Method DOM

---
+++ app.js
@@ -0,0 +1,37 @@
+// DOM Manipulation
+
+
+
+// GetElementById()
+
+/*const title = window.document.getElementById('main-heading');
+
+console.log(title);*/
+
+
+// GetElementByClassName()
+
+/*const listItem = document.getElementsByClassName('list-items');
+
+console.log(listItem);*/
+
+
+// GetElementsByTagName()
+
+/*const listItem = window.document.getElementsByTagName('li');
+
+console.log(listItem);*/
+
+
+// querySelector()
+
+/*const container = window.document.querySelector('div.container');
+
+console.log(container)*/
+
+
+// querySelectorAll()
+
+const container = window.document.querySelectorAll('div');
+
+console.log(container)
