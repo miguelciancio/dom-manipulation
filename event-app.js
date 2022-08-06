9d1125531deec1e48e46551d4a4dc127819c0ffe
miguelciancio <mciancio.figueiredo@gmail.com>
2022-08-06 09:05:59 +0100

    Event listener - Example.

--- event-app.js
+++ event-app.js
@@ -7,7 +7,7 @@
 
 window.document.addEventListener("click", function) -> Example of sintaxe. 
 
-*/
+
 
 const buttonTwo = window.document.querySelector('.btn-2');
 
@@ -16,10 +16,12 @@ function alertBtn() {
 };
 
 buttonTwo.addEventListener("click", alertBtn);
+*/
+
 
 /*
     MOUSEOVER EVENT
-*/
+
 
 const newBackgroundColor = window.document.querySelector('.box-3');
 
@@ -28,6 +30,27 @@ function changeBgColor() {
 };
 
 newBackgroundColor.addEventListener("mouseover", changeBgColor);
+*/
 
+/*
+    DOM MANIPULATION
 
+    EXAMPLE
+*/
 
+// REVEAL EVENT
+
+const revealBtn = window.document.querySelector('.reveal-btn');
+
+const hiddenContent = window.document.querySelector('.hidden-content');
+
+function revealContent() {
+
+    if (hiddenContent.classList.contains('reveal-btn')) {
+        hiddenContent.classList.remove('reveal-btn');
+    } else {
+        hiddenContent.classList.add('reveal-btn');
+    }
+}
+
+revealBtn.addEventListener('click', revealContent);
